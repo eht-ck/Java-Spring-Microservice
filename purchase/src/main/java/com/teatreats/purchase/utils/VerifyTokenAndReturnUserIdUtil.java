e3fa80f368b8199eeef67cad8aaa0e27dc6f5972
eht-ck <chandresh.beas@gmail.com>
2025-02-28 12:22:23 +0530

    fix: logs and error handling

--- purchase/src/main/java/com/teatreats/purchase/utils/VerifyTokenAndReturnUserIdUtil.java
+++ purchase/src/main/java/com/teatreats/purchase/utils/VerifyTokenAndReturnUserIdUtil.java
@@ -3,9 +3,10 @@ package com.teatreats.purchase.utils;
 import com.teatreats.purchase.customexception.UnauthorizedException;
 import com.teatreats.purchase.service.JWTService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+@Slf4j
 @Component
 public class VerifyTokenAndReturnUserIdUtil {
 
@@ -15,6 +16,7 @@ public class VerifyTokenAndReturnUserIdUtil {
     public int validateToken(HttpServletRequest request) throws UnauthorizedException {
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            log.warn("Token not passed.");
             throw new UnauthorizedException("Missing or invalid Authorization header");
         }
         String token = authHeader.substring(7);
@@ -24,6 +26,7 @@ public class VerifyTokenAndReturnUserIdUtil {
     public boolean validateAdminToken(HttpServletRequest request) throws UnauthorizedException {
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            log.warn("Token not passed");
             throw new UnauthorizedException("Missing or invalid Authorization header");
         }
         String token = authHeader.substring(7);
